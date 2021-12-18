0698148fe15641b6ef2802f2e2f3949499075747
Danish Saleem <danish_s@outlook.com>
2021-12-18 17:19:37 +0500

    update files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from 'sections/banner';
-import Services from 'sections/services';
-import Testimonials from 'sections/testimonials';
-import OurTeam from 'sections/our-team';
-import OtherServices from 'sections/other-services';
-import WhyUs from 'sections/why-us';
-import SubscribeUs from 'sections/subscribe-us';
-import Blog from 'sections/blog';
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
+import SEO from "components/seo";
+import Layout from "components/layout";
+import Banner from "sections/banner";
+import Services from "sections/services";
+import Testimonials from "sections/testimonials";
+import OurTeam from "sections/our-team";
+import OtherServices from "sections/other-services";
+import WhyUs from "sections/why-us";
+import SubscribeUs from "sections/subscribe-us";
+import Pricing from "sections/pricing";
 
 export default function IndexPage() {
   return (
@@ -25,7 +25,7 @@ export default function IndexPage() {
         <OurTeam />
         <OtherServices />
         <WhyUs />
-        <Blog />
+        <Pricing />
         <SubscribeUs />
       </Layout>
     </ThemeProvider>
